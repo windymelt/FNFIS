9edcc2af2eda7558f07d7e9fda886681e8962c1a
Qwilas Windymelt <qwilas@mail.3qe.us>
2012-01-30 00:16:08 +0900

    next

--- FNFI/src/jp/freenet/FNFI/client/FNFIClient.scala
+++ FNFI/src/jp/freenet/FNFI/client/FNFIClient.scala
@@ -1,5 +1,15 @@
 package jp.freenet.FNFI.client
+import sun.org.mozilla.javascript.debug.Debugger
 
 object FNFIClient extends App {
-
+  System.err.println("DEBUG START")
+  Console.println("FNFI Client")
+  val F = new FNFIController(new FNFIConnect("cirno"))
+  val hello = F.ClientHello("FNFIS", "2.0")
+  Console.println("Supported Language is " + hello.items("NodeLanguage"))
+  //Test: get FNFI.dtd
+  val fnfidtd = F.ClientGet("CHK@3tKHs~gb-PgmhXnfuJ00PD5RuIAykBzisNM9x542nqc,36eqIi3A7a-kWqPg2lVtdKCsi5J51K~sUniGkiXf154,AAIC--8/FNFI01.dtd",
+		  "brobtextzeromoebius")
+		  Console.println("FNFI01.dtd:\n" + fnfidtd.data.toString())
+true
 }
--- FNFI/src/jp/freenet/FNFI/client/libFNFI.scala
+++ FNFI/src/jp/freenet/FNFI/client/libFNFI.scala
@@ -2,34 +2,74 @@
  *
  */
 package jp.freenet.FNFI.client
+import java.io.ByteArrayInputStream
+import java.io.InputStreamReader
 
 /**
  * @author qwilas
  *
  */
-class FNFIMessage(header:String, message:String) {
+class FNFIMessage(private val header:String, private val message:String) {
   val head = header
   var items = Map.empty[String, String]
   message.lines.foreach((s:String) => items = items.updated(s.split("=")(0), s.split("=")(1)))
   	
-  override def toString():String = {
-      var s:String = head
-      items foreach ((i) => s = s + i._1 + "=" + i._2 + "\n")
-      s + "EndMessage"
+def toString(insertEndMessage:Boolean = true):String = {
+      var s:String = head + "\n"
+      items foreach ((i) => if (i._2 == "") {s = s + i._1 + "\n"} else {s = s + i._1 + "=" + i._2 + "\n"})
+      if (insertEndMessage) {return s + "EndMessage\n"} else {return s}
     }
 }
+
+class FNFIMessageWithData(header:String, message:String, datablock:Array[Byte])
+extends FNFIMessage(header, message) {
+  val data:Array[Byte] = datablock
+  override def toString():String = {
+    var s:String = head + "\n"
+      items foreach ((i) => if (i._2 == "") {s = s + i._1 + "\n"} else {s = s + i._1 + "=" + i._2 + "\n"})
+      s + data
+}
+}
+
 class FNFIController(C:FNFIConnect) {
    def makeMessagefromMap(name:String, m:Map[String, String]):FNFIMessage = {
      var s:String = ""
-     m foreach ((mi) => s = s + mi._1 + "=" + mi._2 + "\n")
+     m foreach ((mi) => if (mi._2 == "") {s = s + mi._1 + "\n"} else {s = s + mi._1 + "=" + mi._2 + "\n"})
      new FNFIMessage(name, s.init)
   }
-   def sendMessagefromMap(title:String, m:Map[String,String]) = {
-     C.sendMessage(makeMessagefromMap(title, m))
+   def sendMessagefromMap(title:String, m:Map[String,String], insertEndMessage:Boolean = true) = {
+     C.sendMessage(makeMessagefromMap(title, m), insertEndMessage)
+   }
+   def exchangeMessagefromMap(title:String, m:Map[String,String], insertEndMessage:Boolean = true):FNFIMessage = {
+     C.sendMessage(makeMessagefromMap(title, m), insertEndMessage)
+     
+     C.recvMessage()
+   }
+   def exchangeMessagefromMaptoData(title:String, m:Map[String,String]): FNFIMessageWithData = {
+     C.sendMessage(makeMessagefromMap(title, m), true)
+     C.recvMessagewithData()
    }
 	def ClientHello(name:String, expectedversion:String) = {
 	  val a = Map("Name"->name, "ExpectedVersion"->expectedversion)
-	  sendMessagefromMap("ClientHello", a)
+	  exchangeMessagefromMap("ClientHello", a, true)
+	}
+	def ClientPut(data:ByteArrayInputStream, identifier:String, key:String = "CHK@",
+	    mime:String = "application/octet-stream") = {
+	  val a = Map("URI" -> key, "Metadata.ContentType" -> mime, "Identifier" -> identifier,
+	      "Data" -> "", data.toString() -> "")
+	      exchangeMessagefromMap("ClientPut", a, false)
+	  
+	}
+	def ClientGet(URI:String, Identifier:String, Priority:Short = 4):FNFIMessageWithData = {
+	  val a = Map("URI" -> URI, "Identifier" -> Identifier, "PriorityClass" -> Priority.toString())
+	  exchangeMessagefromMap("ClientGet", a)
+	  C.recvMessagewithData()
+	}
+	def GetNode(GiveOpennetRef:Boolean = false, WithPrivate:Boolean = false, WithVolatile:Boolean = false): FNFIMessage = {
+	  val a = Map("GiveOpennetRef" -> GiveOpennetRef.toString(),
+	      "WithPrivate" -> WithPrivate.toString(),
+	      "WithVolatile" -> WithVolatile.toString())
+	  exchangeMessagefromMap("GetNode", a, true)
 	}
 }
 final class FNFIConnect(address:String, port:Integer=9481){
@@ -37,26 +77,66 @@ import java.io._
 import java.net.{InetAddress,ServerSocket,Socket,SocketException}
 import java.util.Random
 
-private val ia:java.net.InetAddress = null
-private val socket:java.net.Socket = null
-private val out:java.io.ObjectOutputStream = null
-private val in:java.io.ObjectInputStream = null
 
-try {
-      val ia = InetAddress.getByName(address)
-      val socket = new Socket(ia, port)
-      val out = new ObjectOutputStream(new DataOutputStream(socket.getOutputStream()))
-      val in = new DataInputStream(socket.getInputStream())
+	var socket = new Socket(address, port)
+	socket.setTcpNoDelay(true)
+	var out = socket.getOutputStream
 
-      out.close()
-      in.close()
-      socket.close()
-} catch {
-      case e: IOException => e.printStackTrace()
-}
-def sendMessage(msg:FNFIMessage) = {
-    out.writeObject(msg.toString)
+	var in = new DataInputStream(socket.getInputStream())
+
+	System.err.println("ATTEMPTING TO CONNECT:" + address)
+	//System.err.println("SOCK:" + socket.toString() + " STAT:" + socket.isConnected())
+
+def sendMessage(msg:FNFIMessage, insertEndMessage:Boolean = true) = {
+	System.err.println(msg.toString(true))
+	val code = msg.toString(true)
+	
+    out.write(code.getBytes())
     out.flush()
+}
+def recvMessage():FNFIMessage = {
+  System.err.println("Receiving")
+  var strTmp: String = ""
+  var aline:String = ""
+  while (in.available() == 0){/*System.err.print(".")*/}//Wait for reply
+  while (in.available() != 0 && aline != "EndMessage"){
+	aline = in.readLine()
+	strTmp = strTmp + aline + "\n"
+	
+  }
+  val head:String = strTmp.split("\n")(0)
+  
+  val body:String = strTmp.substring(strTmp.indexOf("\n") + 1, strTmp.indexOf("EndMessage") - 1)
+  if (head == "ProtocolError") throw new Exception("ProtocolError:\n" + body)
+  System.err.println("head: " + head)
+  System.err.println("body: " + body)
+
+  new FNFIMessage(head, body)
+}
+def recvMessagewithData():FNFIMessageWithData = {
+  System.err.println("Receiving data")
+  var strTmp: String = ""
+  while (in.available() == 0){/*System.err.print(".")*/}//Wait for reply
+  while (in.available() != 0){
+	
+	strTmp = strTmp + in.readLine() + "\n"
+  }
+  val head:String = strTmp.split("\n")(0)
+  System.err.println("Head: " + head)
+  if (head == "ProtocolError") throw new Exception("ProtocolError:\n" + strTmp.substring(strTmp.indexOf("\n") + 1, strTmp.indexOf("EndMessage") - 1))
+  val body:String = strTmp.substring(strTmp.indexOf("\n") + 1, strTmp.indexOf("\nData\n") + 1)
+  val datastartindex = strTmp.indexOf("\nData\n") + 7
+  System.err.println("Body:\n" + body)
+  System.err.println("DataStart: " + datastartindex)
+  System.err.println("Datalength:" + (strTmp.length() - datastartindex + 1).toString())
+  val data:Array[Byte] = strTmp.substring(datastartindex - 1, strTmp.length()).getBytes()
+  System.err.println("Data:\n" + strTmp.substring(datastartindex - 1, strTmp.length()))
+  
+  new FNFIMessageWithData(head, body, data)
+}
+def exchangeMessage(msg:FNFIMessage):FNFIMessage = {
+  sendMessage(msg)
+  recvMessage()
 }
 private def close(){
   try {
